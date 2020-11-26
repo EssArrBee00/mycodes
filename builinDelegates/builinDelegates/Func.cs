768e9ffb63d2c59d55e06c560f665dd5a6c16e3b
EssArrBee00 <74861952+EssArrBee00@users.noreply.github.com>
2020-11-26 16:25:35 +0500

    delgate Codes 01

---
+++ builinDelegates/builinDelegates/Func.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+/*
+Func is a generic delegate that encapsulates a method that can accept parameters 
+and return some value.
+
+There are 17 Func delegates defined in the class library. 
+The first delegate cannot accept parameters and  can only return value
+Other Func delegate can take upto 16 parameters and one return value. 
+The last parameter is always the return type.
+
+ */
+
+
+namespace builinDelegates
+{
+    class Func
+    {
+        public static int sum(int a,int b)
+        {
+            Console.WriteLine($"this funtion sums two values {a+b}");
+            return a + b;
+
+        }
+
+
+
+
+        static void Main(string[] args)
+        {
+            Console.Write("introducing Func delegate\n");
+
+            Func<int, int, int> delgateFunc = sum;
+            delgateFunc(2, 3);
+
+
+        }
+    }
+}
---
+++ builinDelegates/builinDelegates/Program.cs
@@ -0,0 +1,70 @@
+﻿//using System;
+
+///* using System;
+///*
+//Microsoft introduced some pre-built delegates so that we don't have to declare delegates
+//every time. Action is one of the pre-built delegates.
+
+//Action in C# represents a delegate that has void return type and optional parameters. 
+//There are two variants of Action delegate.
+
+//1-Action
+//2-Action<in T>
+
+//1- Action
+//First variant is non-generic delegate that takes no parameters and has void return type. 
+//In this Action delegate, we can store only those methods which has no parameters and 
+//void return type.
+
+//2- Action<in T>
+
+//The second variant is a family of 16 generic delegates.
+//These delegates can take upto 16 parameters and all have void return type.
+//*/
+
+
+
+
+//namespace builinDelegates
+//{
+//   class Program
+//   {
+
+//        public static void DoWorkWithOneParameter(int arg)
+//        {
+//            Console.WriteLine(arg);
+//        }
+
+//        public static void DoWorkWithTwoParameters(int arg1, int arg2)
+//        {
+//            Console.WriteLine(arg1 + "-" + arg2);
+//        }
+
+//        public static void DoWorkWithThreeParameters(int arg1, int arg2, int arg3)
+//        {
+//            Console.WriteLine(arg1 + "-" + arg2 + "-" + arg3);
+//        }
+//        public static void DoWork()
+//        {
+//            Console.WriteLine("Hi, I am doing work with zero parameters.");
+//        }
+
+
+
+//        static void Main(string[] args)
+//       {
+//           Console.WriteLine("Hello World!");
+//            Action Zeroparam = new Action(DoWork); //declaring action delegate to dowork funtion
+//            Zeroparam();
+
+//            Action<int> Oneparam = DoWorkWithOneParameter;
+//            Oneparam(1);
+
+//            Action<int, int> Twoparam = DoWorkWithTwoParameters;
+//            Twoparam(1, 2);
+
+//            Action<int, int, int> Threeparam = new Action <int,int,int>(DoWorkWithThreeParameters);
+//            Threeparam(1, 2, 3);
+//       }
+//   }
+//}
